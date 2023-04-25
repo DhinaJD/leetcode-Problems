9895b38da0dd9b9d4adabae5ec8d4757f0273fb0
DhinaJD <dhinakaran.jayaraj17@gmail.com>
2023-04-25 08:49:16 +0530

    longest prefix roman

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // 13. Roman to Integer
 
-
 var romanToInt = function(s) {
   const map = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
 
@@ -14,9 +13,34 @@ var romanToInt = function(s) {
     }
 
   });
+};
+// romanToInt("XXX");
+
+// 14. Longest Common Prefix
+
+/*
+  horizontal methord and //!vertical methord
+  vertical methord have better time complexity 
+*/
 
 
+var longestCommonPrefix = function(strs) {
+
+    let prefix = "";
+    if(strs.length === 0 ) return prefix
+    for(let i=0; i<strs[0].length; i++){
+      let charcaters = strs[0][i];  //f
+      for(let j = 0 ; j< strs.length; j++){
+        if(strs[j][i] !== charcaters) return prefix
+      }
+      prefix += charcaters;
+    }
+
+    return prefix
 };
 
-// 
-romanToInt("XXX");
+strs = ["flower","flow","flight"]
+longestCommonPrefix(strs);
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
